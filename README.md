614909be158b123ba7b841ea06bdb2810e6f42c7
Peanutt42 <p3anutt42@gmail.com>
2026-08-12 00:51:23 +0200

    moved to 11ty + vite for templating and bundling

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-# [Peanutt42.github.io](https://peanutt42.github.io/)
+# [peternhennig.de](https://peternhennig.de/)
 
+Uses 11ty for html templating and vite for bundling.
 
 Personal portfolio website
+
+## Dev
+
+```bash
+npm run dev
+```
+
+## Bundle
+
+```bash
+npm run build
+```
+
+Output dir is `dist`
---
+++ eleventy.config.mjs
@@ -0,0 +1,31 @@
+import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import inline from "@zhoumutou/vite-plugin-inline";
+
+export default function (eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("src/style.css");
+	eleventyConfig.addPassthroughCopy("src/ardemu_web");
+	eleventyConfig.ignores.add("src/ardemu_web/**");
+	eleventyConfig.addPassthroughCopy("src/icons");
+	eleventyConfig.addPassthroughCopy("src/images");
+
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+		svgShortCircuit: "size"
+	});
+
+	eleventyConfig.addPlugin(EleventyVitePlugin, {
+		viteOptions: {
+			plugins: [
+				inline()
+			],
+			build: {},
+		},
+	});
+
+	return {
+		dir: {
+			input: "src",
+			output: "dist",
+		},
+	};
+};
